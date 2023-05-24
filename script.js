ae994e085de1679f959d4e5661f496b6b5d9f499
SametKiybik <samettemas05@gmail.com>
2023-05-24 19:20:49 +0300

    moviesContent

--- script.js
+++ script.js
@@ -10,13 +10,48 @@ fetch("data.json")
         <img class="i" src="${element.img}" alt="resim">        
         <p class="pAd">${element.adi}</p>
         <p class="pBilgi">İmdb:${element.imdb} Yılı:${element.yil}</p>
-        
-        
-        </span>
-
-        
+        </span>        
         `
     })
 
-    document.querySelector(".content").innerHTML=yazdir.join()
+    document.querySelector(".content").innerHTML=yazdir.join("")
+
+
+    
 })
+
+fetch("movies.json")
+.then(response=>response.json())
+.then(value=>{
+    
+    let movie=value.map(element=>{
+        
+        return`
+        <div class="col-5 mt-3 d-flex justify-content-center">
+        <div class="movieBox border">
+        <img class="movieBoxImg" src="${element.img}" alt="resim">        
+        <p>${element.adi}</p>
+        <p>İmdb:${element.imdb} Yılı:${element.yil}</p>
+        </div>
+        
+         
+        </div>      
+        `
+    })
+
+    document.querySelector(".bottomRow").innerHTML=movie.join("")
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
