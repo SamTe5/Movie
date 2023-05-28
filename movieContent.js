13ac9095c89bbcf815c0c61878550b2207be81b3
SametKiybik <samettemas05@gmail.com>
2023-05-28 16:42:45 +0300

    movie

--- movieContent.js
+++ movieContent.js
@@ -14,7 +14,7 @@ fetch("movies.json")
             return `
             <div class="row mt-5">
                 <div class="col-8 text-white">
-                    <div class="p mt-3 w-50 d-none position-absolute"></div>
+                    
                     <h3>${value.yil}</h3>
                     <p class="mt-5" style="font-size: 12px; color:grey;">GENRE</p>
                     <p>${value.tur} <span class="ps-5" style="font-size:1.5rem;">${value.imdb}</span><span style="font-size: 12px; color:grey;" class="ps-2">IMDB</span></p>
@@ -36,21 +36,26 @@ fetch("movies.json")
 
 
         /*BG ımage*/
-        document.querySelector(".bgImageId").style.backgroundImage = `linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.3)),url(${gelenMovieDizi[0].img})`
-        document.querySelector(".bgImageId").style.backgroundRepeat = "no-repeat"
-        document.querySelector(".bgImageId").style.backgroundSize = "cover";
-        document.querySelector(".bgImageId").style.backgroundPosition = "center";
+        let bg=document.querySelector(".bgImageId")
+        bg.style.backgroundImage = `linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.3)),url(${gelenMovieDizi[0].img})`
+        bg.style.backgroundRepeat = "no-repeat"
+        bg.style.backgroundSize = "cover";
+        bg.style.backgroundPosition = "center";
 
         //film oynatmak
         let buton= document.querySelector(".play")
+        
+        
 
         buton.addEventListener("click",function(){
+            let fContainer=document.querySelector(".dsplay").style.display="none"
+            let containerim=document.createElement("div")
+            containerim.className="container-fluid"
             const iframe=document.createElement("iframe")
-            iframe.className="run w-100"
-            iframe.setAttribute("src",gelenMovie.link)
-            document.querySelector(".p").style.display = "block"
-            document.querySelector(".p").appendChild(iframe)
-            buton.disabled=true
+            iframe.className="run w-100 vh-100"
+            iframe.setAttribute("src",gelenMovie.link)           
+            document.body.appendChild(iframe)
+                  
             
 
             
