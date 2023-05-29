ca2c99be1e7cbcccaf24be76e87365e89940240a
SametKiybik <samettemas05@gmail.com>
2023-05-29 11:30:06 +0300

    top10 navbar

--- top10.js
+++ top10.js
@@ -1,11 +1,11 @@
 let conf= document.querySelector(".tCon")
 conf.className="w-100 vh-100 bgImage"
 let con=document.createElement("div")
-con.className="container pt-3"
+con.className="container pt-2"
 conf.appendChild(con)
 
 let nav=document.createElement("navbar")
-nav.className="d-flex"
+nav.className="navbar navbar-expand-lg "
 con.appendChild(nav)
 
 let a=document.createElement("img")
@@ -14,9 +14,10 @@ a.className="logo"
 nav.appendChild(a)
 
 let topIcerik=document.createElement("div")
+topIcerik.className="collapse navbar-collapse"
 nav.appendChild(topIcerik)
 let topUl=document.createElement("ul")
-topUl.className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-center"
+topUl.className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-center w-100 h-100"
 topIcerik.appendChild(topUl)
 
 let li1=document.createElement("li")
@@ -31,6 +32,22 @@ let li3=document.createElement("li")
 li3.className="nav-item"
 topUl.appendChild(li3)
 
+let aItem1=document.createElement("a")
+aItem1.className="nav-link topTenColorActive"
+aItem1.innerHTML="TOP 10"
+li1.appendChild(aItem1)
+
+let aItem2=document.createElement("a")
+aItem2.className="nav-link"
+aItem2.setAttribute("href","#")
+aItem2.innerHTML="Movies"
+li2.appendChild(aItem2)
+
+let aItem3=document.createElement("a")
+aItem3.className="nav-link"
+aItem3.setAttribute("href","#")
+aItem3.innerHTML="Series"
+li3.appendChild(aItem3)
 
 
 
