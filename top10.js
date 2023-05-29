f67106347d240dc88c26ef0dc242b8418b2fa8c3
SametKiybik <samettemas05@gmail.com>
2023-05-29 09:27:33 +0300

    top10

---
+++ top10.js
@@ -0,0 +1,9 @@
+let conf= document.querySelector(".tCon")
+conf.className="w-100 vh-100 bgImage"
+let con=document.createElement("div")
+con.className="container"
+conf.appendChild(con)
+
+let nav=document.createElement("navbar")
+con.appendChild(nav)
+
